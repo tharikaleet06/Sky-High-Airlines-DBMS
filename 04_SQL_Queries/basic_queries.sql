6d131c5d0f853055463ad6a4cefd6a09e1a38aa3
Tharika P <tharikaleet06@gmail.com>
2026-08-24 22:39:34 +0530

    Update basic_queries.sql

--- 04_SQL_Queries/basic_queries.sql
+++ 04_SQL_Queries/basic_queries.sql
@@ -1,1 +1,41 @@
+-- =========================================================
+-- SKY HIGH AIRLINES DATABASE
+-- BASIC QUERIES
+-- Queries 1 to 5
+-- =========================================================
 
+
+-- 1. List all flights departing from New York (JFK)
+
+SELECT *
+FROM FLIGHT
+WHERE source_airport = 'JFK';
+
+
+-- 2. Find passengers who have booked Business Class seats
+
+SELECT *
+FROM SEAT
+WHERE seat_class = 'BUSINESS';
+
+
+-- 3. Retrieve flights scheduled for Next Monday
+
+SELECT *
+FROM FLIGHT
+WHERE TRUNC(departure_datetime) =
+      NEXT_DAY(TRUNC(SYSDATE), 'MONDAY');
+
+
+-- 4. List bookings that have been Cancelled
+
+SELECT *
+FROM BOOKING
+WHERE booking_status = 'CANCELLED';
+
+
+-- 5. Find airports located in France
+
+SELECT *
+FROM AIRPORT
+WHERE country = 'France';
